eddf1c001a5d0734697fee920b8ec8e357208686
horothesun <horothesun@gmail.com>
2022-12-23 01:25:36 +0000

    aws_lambda_function fix

--- lambda.tf
+++ lambda.tf
@@ -4,6 +4,6 @@ resource "aws_lambda_function" "lambda" {
   timeout       = local.http_timeout_in_seconds - 1
   memory_size   = var.lambda_memory_MB
   architectures = ["x86_64"]
-  image_uri     = var.initial_setup ? null : "${aws_ecr_repository.ecr_repo.repository_url}@${data.aws_ecr_image.ecr_image.id}"
+  image_uri     = var.initial_setup ? null : "${aws_ecr_repository.ecr_repo.repository_url}@${data.aws_ecr_image.ecr_image[0].id}"
   package_type  = var.initial_setup ? null : "Image"
 }
