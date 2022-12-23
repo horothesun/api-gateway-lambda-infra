4ebe7643fe4d80d95264444b43c32ba9e327e617
horothesun <horothesun@gmail.com>
2022-12-23 01:47:39 +0000

    Lambda fix

--- lambda.tf
+++ lambda.tf
@@ -7,4 +7,6 @@ resource "aws_lambda_function" "lambda" {
   package_type  = var.initial_setup ? "Zip" : "Image"
   image_uri     = var.initial_setup ? null : "${aws_ecr_repository.ecr_repo.repository_url}@${data.aws_ecr_image.ecr_image[0].id}"
   filename      = var.initial_setup ? "placeholder.zip" : null
+  handler       = var.initial_setup ? "index.test" : null
+  runtime       = var.initial_setup ? "nodejs18.x" : null
 }
