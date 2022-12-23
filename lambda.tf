d1d0aa747f26e22b605d485d4e8a5fadc1eb5697
horothesun <horothesun@gmail.com>
2022-12-23 01:28:31 +0000

    Lambda fix

--- lambda.tf
+++ lambda.tf
@@ -5,5 +5,5 @@ resource "aws_lambda_function" "lambda" {
   memory_size   = var.lambda_memory_MB
   architectures = ["x86_64"]
   image_uri     = var.initial_setup ? null : "${aws_ecr_repository.ecr_repo.repository_url}@${data.aws_ecr_image.ecr_image[0].id}"
-  package_type  = var.initial_setup ? null : "Image"
+  package_type  = "Image"
 }
